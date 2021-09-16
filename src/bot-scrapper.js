3748d741bd5bf7ed98569564d2804fd33beff81d
Matias Samuel Bustamante <samuelbustamante999@gmail.com>
2021-09-16 12:53:19 -0300

    borrado de archivo test.js

--- src/bot-scrapper.js
+++ src/bot-scrapper.js
@@ -48,3 +48,21 @@ funcion.ejecutarBot = async () => {
 
 
 module.exports = funcion;
+
+// fs.readFile('/usr/src/app/src/public/peliculas.json', async (err, data) => {
+//     if (err) throw err;
+//     let peliculas = JSON.parse(data);
+//     for(pelicula of peliculas) {
+//         const {nombre, director, URL_Imagen, genero, sinopsis} = pelicula;
+//         const nuevaPelicula = new Pelicula({
+//             nombre : nombre,
+//             director : director,
+//             URL_Imagen : URL_Imagen,
+//             genero : genero,
+//             sinopsis : sinopsis
+//         });
+//         await nuevaPelicula.save();
+//     }
+
+//     return res.status(200).json("Peliculas agregadas correctamente");
+// });
--- src/controllers/pelicula.controller.js
+++ src/controllers/pelicula.controller.js
@@ -2,7 +2,6 @@ const indexController = {};
 const puppeteer       = require('puppeteer');
 const {ejecutarBot}   = require('../bot-scrapper');
 const Pelicula        = require('../models/Pelicula');
-const fs              = require('fs');
 
 indexController.obtenerPeliculas = (req, res) => {
     res.status(200).json({
@@ -19,23 +18,7 @@ indexController.obtenerPeliculaPorId = (req, res) => {
 
 indexController.agregarPelicula = async (req, res) => {
 
-        fs.readFile('/usr/src/app/src/public/peliculas.json', async (err, data) => {
-            if (err) throw err;
-            let peliculas = JSON.parse(data);
-            for(pelicula of peliculas) {
-                const {nombre, director, URL_Imagen, genero, sinopsis} = pelicula;
-                const nuevaPelicula = new Pelicula({
-                    nombre : nombre,
-                    director : director,
-                    URL_Imagen : URL_Imagen,
-                    genero : genero,
-                    sinopsis : sinopsis
-                });
-                await nuevaPelicula.save();
-            }
         
-            return res.status(200).json("Peliculas agregadas correctamente");
-        });
 };
 
 indexController.borrarPelicula = (req, res) => {
